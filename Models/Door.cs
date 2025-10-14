d3457a2726259ae63aaed461e24f32d971b55620
Francisco <franciscot.ussa2003@gmail.com>
2025-10-14 06:26:39 -0500

    feat: Implement the Door class

---
+++ Models/Door.cs
@@ -0,0 +1,13 @@
+using DungeonsAdventure.Interfaces;
+using System;
+
+namespace DungeonsAdventure.Models
+{
+    public class Door : IInteractive
+    {
+        public void Interact()
+            {
+                Console.WriteLine("You try to open the door. It's locked.");
+            }
+    }
+}
