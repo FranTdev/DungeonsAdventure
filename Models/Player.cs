4c8f0ade076549e26c55e7a8ce11e9bb54c32f19
Francisco <franciscot.ussa2003@gmail.com>
2025-10-15 11:15:31 -0500

    feat: Create Player class to manage game state

---
+++ Models/Player.cs
@@ -0,0 +1,15 @@
+namespace DungeonsAdventure.Models
+{
+    public class Player
+    {
+        public string Name { get; set; }
+
+        public Room CurrentRoom { get; set; }
+
+        public Player (string name, Room startingRoom)
+        {
+            Name = name;
+            CurrentRoom = startingRoom;
+        }
+    }
+}
